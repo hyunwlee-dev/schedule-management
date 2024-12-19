e7bd3103d1bb985106adbf1a35a18c981bd40250
hyunwlee <hwlee3928@gmail.com>
2024-12-19 11:49:47 +0900

    move(css-style): styles 폴더 [font, globals.css] 외부로 이동

--- styles/font.ts
+++ styles/font.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local';
 
 export const pretendardFont = localFont({
-  src: '../../public/fonts/PretendardVariable.woff2',
+  src: '../public/fonts/PretendardVariable.woff2',
   display: 'swap',
   weight: '45 920',
   variable: '--font-pretendard',
