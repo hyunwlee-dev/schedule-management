6aa8db36c5118e434f2c4406183ca7e51f1650ba
hyunwlee <hwlee3928@gmail.com>
2024-12-20 12:53:17 +0900

    feat(component): header

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { ArrowRightStartOnRectangleIcon, Bars3Icon } from '@heroicons/react/16/solid';
+
+import { useSidebarContext } from './commons/sidebar/SidebarProvider';
+
+function Header() {
+  const { handleToggle } = useSidebarContext();
+
+  return (
+    <header className="flex justify-between md:justify-end px-[16px] pb-[18px] pt-[16px] align-middle">
+      <button onClick={handleToggle} className="block md:hidden">
+        <Bars3Icon className="text-slate-600 size-6" />
+      </button>
+
+      <button>
+        <ArrowRightStartOnRectangleIcon className="text-slate-600 size-6" />
+      </button>
+    </header>
+  );
+}
+
+export default Header;
