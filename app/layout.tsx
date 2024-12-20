881dcad5451b3608c1fa20ecc9e58c8524d2b38c
hyunwlee <hwlee3928@gmail.com>
2024-12-20 20:39:28 +0900

    chore(package): react-query, react-query-devtools setting

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { pretendardFont } from '@styles/font';
 import '@styles/globals.css';
 import cn from '@utils/cn';
 
+import QueryProvider from '@/components/providers/QueryProvider';
+
 export const metadata: Metadata = {
   title: 'schedule management',
   description: '일정관리 캘린더, 실시간 채팅 개인 프로젝트',
@@ -22,16 +24,18 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={cn('flex min-h-screen', pretendardFont.className)}>
-        <SidebarProvider>
-          <ScheduleSidebar />
-          <div className="flex flex-auto flex-col">
-            <Header />
-            <main className="flex-auto flex justify-center mt-20">
-              {children}
-              {modal}
-            </main>
-          </div>
-        </SidebarProvider>
+        <QueryProvider>
+          <SidebarProvider>
+            <ScheduleSidebar />
+            <div className="flex flex-auto flex-col">
+              <Header />
+              <main className="flex-auto flex justify-center mt-20">
+                {children}
+                {modal}
+              </main>
+            </div>
+          </SidebarProvider>
+        </QueryProvider>
       </body>
     </html>
   );
---
+++ components/providers/QueryProvider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { PropsWithChildren, useState } from 'react';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+function QueryProvider({ children }: PropsWithChildren) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
