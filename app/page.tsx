61983bfc66ee3c4101395ab3afacaa70b7581cfc
hyunwlee <hwlee3928@gmail.com>
2024-12-21 19:55:50 +0900

    feat(component): calendarBoard, css, page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
-import CustomCalendar from '@components/calendar/CalendarBoard';
+'use server';
+
+import { getSchedules } from '@actions/schedule';
+import CalendarBoard from '@components/calendar/CalendarBoard';
+import { SCHEDULE_DATE_FORMAT } from '@constants/schedule';
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+import { getVisibleRangeDate } from '@utils/getVisibleRangeDate';
+import dayjs from 'dayjs';
 
 async function Home() {
-  return <CustomCalendar />;
+  const queryClient = new QueryClient();
+
+  const { firstVisibleDate, lastVisibleDate } = getVisibleRangeDate(
+    dayjs().startOf('month').format(SCHEDULE_DATE_FORMAT),
+  );
+
+  await queryClient.prefetchQuery({
+    queryKey: [
+      'schedules',
+      {
+        startDate: firstVisibleDate.format(SCHEDULE_DATE_FORMAT),
+        endDate: lastVisibleDate.format(SCHEDULE_DATE_FORMAT),
+      },
+    ],
+    queryFn: () =>
+      getSchedules({
+        startDate: firstVisibleDate.format(SCHEDULE_DATE_FORMAT),
+        endDate: lastVisibleDate.format(SCHEDULE_DATE_FORMAT),
+      }),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <CalendarBoard />
+    </HydrationBoundary>
+  );
 }
 
 export default Home;
--- components/calendar/CalendarBoard.tsx
+++ components/calendar/CalendarBoard.tsx
@@ -3,9 +3,11 @@
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
+import { SCHEDULE_DATE_FORMAT } from '@constants/schedule';
+import { useSchedulesQuery } from '@queries/useSchedulesQuery';
+import { getVisibleRangeDate } from '@utils/getVisibleRangeDate';
 import dayjs from 'dayjs';
-import 'dayjs/locale/ko';
-import Calendar from 'react-calendar';
+import Calendar, { OnArgs, type TileArgs } from 'react-calendar';
 
 import './calendar.css';
 
@@ -13,18 +15,68 @@ type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
-const SCHEDULE_DATE_FORMAT = 'YYYY-MM-DD';
-
 function CalendarBoard() {
   const router = useRouter();
-  const [date, setDate] = useState<Value>(new Date());
+  const [pickedDate, setPickedDate] = useState<Value>(new Date());
+  const [dateRange, setDateRange] = useState<{ startDate: string; endDate: string }>(() => {
+    const { firstVisibleDate, lastVisibleDate } = getVisibleRangeDate(
+      dayjs().startOf('month').format(SCHEDULE_DATE_FORMAT),
+    );
+    return {
+      startDate: firstVisibleDate.format(SCHEDULE_DATE_FORMAT),
+      endDate: lastVisibleDate.format(SCHEDULE_DATE_FORMAT),
+    };
+  });
 
-  const handleDate = (value: Value) => {
-    router.push(`/schedules/${dayjs(value?.toString()).format(SCHEDULE_DATE_FORMAT)}`);
-    setDate(value);
+  const { data: schedules } = useSchedulesQuery(dateRange);
+
+  const titleContent = ({ date, view }: TileArgs) => {
+    if (view !== 'month') return null;
+
+    const formattedDate = dayjs(date).format(SCHEDULE_DATE_FORMAT);
+
+    const matchingSchedules = schedules?.filter((schedule) => {
+      const scheduleStart = dayjs(schedule.start_at).format(SCHEDULE_DATE_FORMAT);
+      const scheduleEnd = dayjs(schedule.end_at).format(SCHEDULE_DATE_FORMAT);
+
+      return formattedDate >= scheduleStart && formattedDate <= scheduleEnd;
+    });
+
+    return (
+      <div className="h-16 text-[10px] border border-slate-500 rounded-b-md">
+        {matchingSchedules?.map((schedule) => (
+          <p key={schedule.id} className="pl-1 text-start truncate">
+            - {schedule.title}
+          </p>
+        ))}
+      </div>
+    );
   };
 
-  return <Calendar value={date} onChange={handleDate} />;
+  const handleDate = (value: Value) => {
+    router.push(`/schedules/${dayjs(value?.toString()).format(SCHEDULE_DATE_FORMAT)}`, { scroll: false });
+    setPickedDate(value);
+  };
+
+  const onActiveStartDateChangeHandler = ({ activeStartDate, view }: OnArgs) => {
+    if (view !== 'month') return;
+    const { firstVisibleDate, lastVisibleDate } = getVisibleRangeDate(
+      dayjs(activeStartDate).format(SCHEDULE_DATE_FORMAT),
+    );
+    setDateRange({
+      startDate: firstVisibleDate.format(SCHEDULE_DATE_FORMAT),
+      endDate: lastVisibleDate.format(SCHEDULE_DATE_FORMAT),
+    });
+  };
+
+  return (
+    <Calendar
+      value={pickedDate}
+      onChange={handleDate}
+      tileContent={titleContent}
+      onActiveStartDateChange={onActiveStartDateChangeHandler}
+    />
+  );
 }
 
 export default CalendarBoard;
